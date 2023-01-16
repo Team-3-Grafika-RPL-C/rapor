2b7d47619e5b8502fae9029e7502cb37a69a2b6d
BagaskaraAdhi <bagaskaraadhi3@gmail.com>
2023-01-16 10:58:17 +0700

    add some file

--- web/app/Config/Routes.php
+++ web/app/Config/Routes.php
@@ -35,7 +35,8 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'c_login::index', ['filter' => 'login']);
+$routes->post('/', 'c_login::loginProcess', ['filter' => 'login']);
 
 /*
  * --------------------------------------------------------------------
