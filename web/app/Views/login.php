8ccc9ac3079d13ae7fdf75c56f1d7c25486877c8
BagaskaraAdhi <bagaskaraadhi3@gmail.com>
2023-01-26 14:41:37 +0700

    Add Login, CSS and JS

--- web/app/Views/login.php
+++ web/app/Views/login.php
@@ -1,51 +1,85 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
-    <!-- Bootstrap -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Rapodig - Login</title>  
+    <!-- Custom styles for this template-->
+    <link rel="stylesheet" href="<?= base_url(); ?>/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  </head>
-  <body>
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg fixed-top py-0" style="position: sticky;">
-          <div class="container-fluid">
-              <a href="" class="navbar-brand mx-md-4 py-0">
-                <img src="<?= base_url(); ?>/assets/rapodig.png" height="55px" alt="Logo Rapodig">
-              </a>
-              <a href="" class="navbar-brand mx-md-4 py-0">
-                <img src="<?= base_url(); ?>/assets/tut-wuri-handayani-7777.png" height="55px" alt="Logo Tut Wuri Handayani">
-              </a>
-          </div>
-      </nav>
-    <!-- Card -->
-    <div class="container d-flex justify-content-center align-item-center flex-column" style="height: 90vh;">
-        <div class="card justify-content-center w-100 align-self-center shadow py-5" style="max-width: 25rem; height:auto; border-radius:16px;">
-            <div class="container justify-content-center d-flex my-auto">
-                <h2 class="mt-3">Selamat Datang!</h2>
-            </div>
-            <div class="container justify-content-center d-flex pb-4">
-                <p style="color: #74838B">Silahkan Masuk Terlebih Dahulu</p>
-            </div>
-            <!-- Isi Form -->
-            <div class="container my-auto pt-2">
-                <form method="POST" action="<?= base_url(); ?>/">
-                    <div class="mb-2 px-4">
-                        <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Nama">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-4">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row" style="height:90vh;">
+                            <div class="col-lg-6 d-none d-lg-block card-right">
+                                <div class="d-flex justify-content-center">
+                                    <div class="m-5 p-5">
+                                        <img class="p-5" src="<?= base_url(); ?>/assets/rapodig-removebg-preview.png" alt="Logo Rapodig">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="p-5 mt-5">
+                                    <div class="text-center">
+                                        <h1 class="bold">Selamat Datang!</h1>
+                                        <h5 class="mb-4">Silahkan masuk terlebih dahulu</h5>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user text-muted"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Username">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <div class="my-5">
+                                            <a href="" class="btn btn-user btn-block" style="background-color: #845EF7; color: white">
+                                                Login
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="mb-3 mx-3">
-                        <label for="password" class="form-label d-flex justify-content-start" >Password</label>
-                        <input type="password" class="form-control" name="password" id="password" placeholder="NIP">
-                    </div>
-                    <div class="container d-flex justify-content-end px-4 pb-4">
-                        <button class="btn" style="min-width: 24vh; min-height: 1rem; border-radius: 16px; background-color: #845EF7; color: white" id="loginBtn" type="submit">Login</button>
-                    </div>
-                </form>
+                </div>
+
             </div>
+
         </div>
+
     </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?= base_url(); ?>/vendor/jquery/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  </body>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?= base_url(); ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?= base_url(); ?>/js/rapodig.min.js"></script>
+
+</body>
+
 </html>
