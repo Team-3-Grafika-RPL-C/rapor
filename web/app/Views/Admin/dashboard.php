a048835acbfcdf705f993c595fc1bce545e702b3
viraalfita <viraalfita1813@gmail.com>
2023-01-28 18:51:22 +0700

    vira

--- web/app/Views/Admin/dashboard.php
+++ web/app/Views/Admin/dashboard.php
@@ -105,14 +105,13 @@
           </a>
           <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-              <h6 class="collapse-header">Login Screens:</h6>
-              <a class="collapse-item" href="login.html">Login</a>
-              <a class="collapse-item" href="register.html">Register</a>
-              <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+              <h6 class="collapse-header">Input Data</h6>
+              <a class="collapse-item" href="login.html">Input Nilai Mapel</a>
+              <a class="collapse-item" href="register.html">Input Nilai Ekstrakurikuler</a>
               <div class="collapse-divider"></div>
-              <h6 class="collapse-header">Other Pages:</h6>
-              <a class="collapse-item" href="404.html">404 Page</a>
-              <a class="collapse-item" href="blank.html">Blank Page</a>
+              <h6 class="collapse-header">Rekap Nilai</h6>
+              <a class="collapse-item" href="404.html">Rekap Nilai</a>
+              <a class="collapse-item" href="blank.html">Rekap Nilai Ekstrakurikuler</a>
             </div>
           </div>
         </li>
