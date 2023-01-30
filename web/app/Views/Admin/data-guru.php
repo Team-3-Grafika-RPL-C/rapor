b47cc70266d955b3d3c0e209bf914efd7e1919ab
viraalfita <viraalfita1813@gmail.com>
2023-01-30 12:56:54 +0700

    manage data guru

--- web/app/Views/Admin/data-guru.php
+++ web/app/Views/Admin/data-guru.php
@@ -18,49 +18,116 @@
                     <thead>
                         <tr>
                             <th class="text-center">No</th>
-                            <th class="text-center">Nama Kelas</th>
-                            <th class="text-center">Tingkat</th>
-                            <th class="text-center">Wali Kelas</th>
+                            <th class="text-center">NIP</th>
+                            <th class="text-center">Nama</th>
+                            <th class="text-center">Alamat</th>
+                            <th class="text-center">Status</th>
                             <th class="text-center">Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>1</td>
-                            <td>1A</td>
-                            <td>Satu</td>
+                            <td>21202020202</td>
                             <td>Dian Retno, S.Pd</td>
-                            <td>
-                                <a href="" class="btn btn-primary rounded" data-toggle="modal">
-                                    <i class="ri-information-fill" data-toggle="tooltip" title="Detail"></i>
-                                </a>
-                                <a href="" class="edit" data-toggle="modal">
-                                    <i class="ri-pencil-fill" data-toggle="tooltip" title="Edit"></i>
-                                </a>
-                                <a href="" class="delete" data-toggle="modal">
-                                    <i class="" data-toggle="tooltip" title="Delete"></i>
-                                </a>
-                            </td>
+                            <td>Jl. Mawar No.11</td>
+                            <td>Aktif</td>
+                            <td></td>
                         </tr>
                         <tr>
                             <td>2</td>
-                            <td>1B</td>
-                            <td>Satu</td>
+                            <td>212020254402</td>
                             <td>Gunawan Dwiyono, S.Pd</td>
+                            <td>Jl. Melati No.1</td>
+                            <td>Aktif</td>
                             <td></td>
                         </tr>
                         <tr>
                             <td>3</td>
-                            <td>2A</td>
-                            <td>Dua</td>
+                            <td>212987020202</td>
                             <td>Solikin, S.Pd</td>
+                            <td>Jl. Comboran No.69</td>
+                            <td>Aktif</td>
                             <td></td>
                         </tr>
                         <tr>
                             <td>4</td>
-                            <td>2B</td>
-                            <td>Dua</td>
+                            <td>55203838202</td>
                             <td>Vira Alfita, M.Pd</td>
+                            <td>Jl. Gadang No.11</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>2120647602</td>
+                            <td>Bagaskara, M.Pd</td>
+                            <td>Jl. Mergosono No.8</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>2126280202</td>
+                            <td>Goh Cafriel Chelsea, S.Pd</td>
+                            <td>Jl. Bululawang No.3</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>21202020202</td>
+                            <td>Faisal Adi, S.Pd</td>
+                            <td>Jl. Kenanga No.11</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>8</td>
+                            <td>212020254402</td>
+                            <td>Adinda Devina, S.Pd</td>
+                            <td>Jl. Bareng No.9</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>212987020202</td>
+                            <td>Wayan, S.Pd</td>
+                            <td>Jl. Teratai No.69</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>55203838202</td>
+                            <td>Anggraeni Tyas, M.Pd</td>
+                            <td>Jl. P.Sayang No.1</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>11</td>
+                            <td>2120647602</td>
+                            <td>Yunia, M.Pd</td>
+                            <td>Jl. Kembang Sepatu No.8</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>12</td>
+                            <td>2126280202</td>
+                            <td>Wawan, S.Pd</td>
+                            <td>Jl. Songgoriti No.3</td>
+                            <td>Aktif</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>13</td>
+                            <td>2126280202</td>
+                            <td>Imam, S.Pd</td>
+                            <td>Jl. Halmahera No.3</td>
+                            <td>Aktif</td>
                             <td></td>
                         </tr>
                     </tbody>
