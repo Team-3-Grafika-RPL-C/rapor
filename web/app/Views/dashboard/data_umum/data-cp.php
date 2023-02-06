bf51cc15e5d57c83267f525c6f03b08198fc3283
BagaskaraAdhi <bagaskaraadhi3@gmail.com>
2023-02-06 12:54:15 +0700

    Add alert

--- web/app/Views/dashboard/data_umum/data-cp.php
+++ web/app/Views/dashboard/data_umum/data-cp.php
@@ -40,7 +40,7 @@
                             <a href="" class="btn btn-warning btn-rounded my-1">
                                 <i class="ri-pencil-fill" data-toggle="tooltip" title="Edit"></i>
                             </a>
-                            <a href="" class="btn btn-danger btn-rounded my-1">
+                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal" data-target="#delete" >             
                                 <i class="ri-delete-bin-7-fill" data-toggle="tooltip" title="Delete"></i>
                             </a>
                         </td>
@@ -53,7 +53,7 @@
                             <a href="" class="btn btn-warning btn-rounded my-1" data-toggle="modal">
                                 <i class="ri-pencil-fill" data-toggle="tooltip" title="Edit"></i>
                             </a>
-                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal">
+                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal" data-target="#delete">
                                 <i class="ri-delete-bin-7-fill" data-toggle="tooltip" title="Delete"></i>
                             </a>
                         </td>
@@ -66,13 +66,34 @@
                             <a href="" class="btn btn-warning btn-rounded my-1" data-toggle="modal">
                                 <i class="ri-pencil-fill" data-toggle="tooltip" title="Edit"></i>
                             </a>
-                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal">
+                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal" data-target="#delete">
                                 <i class="ri-delete-bin-7-fill" data-toggle="tooltip" title="Delete"></i>
                             </a>
                         </td>
                     </tr>
                 </tbody>
             </table>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Delete -->
+<div class="modal fade" id="delete" tabindex="-1" aria-labelledby="alertModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content py-4">
+            <div class="modal-body">
+                <div class="container">
+                    <div class="d-flex justify-content-center">
+                        <p class="p">apakah anda yakin akan menghapus data ini?</p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <div class="row">
+                            <a href="" class="btn btn d-sm-inline-block text-light btn-sm shadow px-4 col-2" style="min-width: 5rem; background-color: #845EF7; border-radius: 16px">ya</a>
+                            <a href="" class="btn btn d-sm-inline-block text-dark btn-sm shadow px-4 col-2"style="min-width: 5rem; background-color: #F8F9F9;border-color:#C8CDD0; border-radius: 16px">tidak</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- web/app/Views/dashboard/data_umum/data-tp.php
+++ web/app/Views/dashboard/data_umum/data-tp.php
@@ -43,7 +43,7 @@
                                 <a href="" class="btn btn-warning btn-rounded my-1">
                                     <i class="ri-pencil-fill" data-toggle="tooltip" title="Edit"></i>
                                 </a>
-                                <a href="" class="btn btn-danger btn-rounded my-1">
+                                <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal" data-target="#delete">
                                     <i class="ri-delete-bin-7-fill" data-toggle="tooltip" title="Delete"></i>
                                 </a>
                             </td>
@@ -60,7 +60,7 @@
                             <a href="" class="btn btn-warning btn-rounded my-1" data-toggle="modal">
                                 <i class="ri-pencil-fill" data-toggle="tooltip" title="Edit"></i>
                             </a>
-                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal">
+                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal" data-target="#delete">
                                 <i class="ri-delete-bin-7-fill" data-toggle="tooltip" title="Delete"></i>
                             </a>
                             </td>
@@ -77,7 +77,7 @@
                             <a href="" class="btn btn-warning btn-rounded my-1" data-toggle="modal">
                                 <i class="ri-pencil-fill" data-toggle="tooltip" title="Edit"></i>
                             </a>
-                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal">
+                            <a href="" class="btn btn-danger btn-rounded my-1" data-toggle="modal" data-target="#delete">
                                 <i class="ri-delete-bin-7-fill" data-toggle="tooltip" title="Delete"></i>
                             </a>
                             </td>
@@ -88,5 +88,27 @@
         </div>
     </div>
 </div>
+
+<!-- Modal Delete -->
+<div class="modal fade" id="delete" tabindex="-1" aria-labelledby="alertModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content py-4">
+            <div class="modal-body">
+                <div class="container">
+                    <div class="d-flex justify-content-center">
+                        <p class="p">apakah anda yakin akan menghapus data ini?</p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <div class="row">
+                            <a href="" class="btn btn d-sm-inline-block text-light btn-sm shadow px-4 col-2" style="min-width: 5rem; background-color: #845EF7; border-radius: 16px">ya</a>
+                            <a href="" class="btn btn d-sm-inline-block text-dark btn-sm shadow px-4 col-2"style="min-width: 5rem; background-color: #F8F9F9;border-color:#C8CDD0; border-radius: 16px">tidak</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- End of Main Content -->
 <?= $this->endsection(); ?>
