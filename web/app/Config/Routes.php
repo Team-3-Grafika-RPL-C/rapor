ac6837dda3a910f62625edefda93368524430664
viraalfita <viraalfita1813@gmail.com>
2023-02-08 09:11:52 +0700

    development session validasi login

--- web/app/Config/Routes.php
+++ web/app/Config/Routes.php
@@ -30,6 +30,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->post('/validasi-login', 'Home::validasi_login');
 $routes->get('/dashboard', 'c_dashboard::index');
 
 $routes->get('/data-kelas', 'c_dataKelas::index');
