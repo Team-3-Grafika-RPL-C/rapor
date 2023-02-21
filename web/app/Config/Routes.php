857bf1f62531ebce36a883e8da961f377a8afb78
FlaBBB <f12345ff67@gmail.com>
2023-02-21 21:33:34 +0700

    login change

--- web/app/Config/Routes.php
+++ web/app/Config/Routes.php
@@ -29,7 +29,7 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
 $routes->get('/dashboard', 'c_dashboard::index');
 
 $routes->get('/data-kelas', 'c_dataKelas::index');
